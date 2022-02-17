7d4793062a15baaaabbb44d02deafe1ae6cef116
SvetlankaBaturkina <melkaya2025sveta@gmail.com>
2022-02-17 14:44:10 +0200

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var array = [bracketsConfig];
+    let brackets = array.join('').replace(/,/g, '');
+    let arrayNew = [];
+
+    for (let bracket of str) {
+            let bracketsIndex = brackets.indexOf(bracket);
+
+
+            if (bracketsIndex % 2 === 0) {
+                
+                if (bracket === brackets[bracketsIndex + 1] && arrayNew[arrayNew.length - 1] === bracketsIndex){
+                  arrayNew.pop();
+                } else if (bracket === brackets[bracketsIndex + 1] && arrayNew[arrayNew.length - 1] !== bracketsIndex) {
+                  arrayNew.push(bracketsIndex)
+                }
+                else{
+                  arrayNew.push(bracketsIndex)
+                }
+            } 
+            else {
+                if (arrayNew.pop() !== bracketsIndex-1) {
+                    return (false);
+                };
+            };
+            
+    };
+        return (arrayNew.length === 0);// your solution
+};
